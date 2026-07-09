ae818a89583985f295f820f116917b2f369329e0
alb2376-stack <alb2376@columbia.edu>
2026-07-08 23:47:28 -0400

    update

---
+++ sketch-2d-1.js
@@ -0,0 +1,58 @@
+// 2D Drawing Sketch - using p5.js instance mode
+var sketch1 = function(p) {
+
+  // All variables are scoped to this instance
+  var canvasWidth = 800;
+  var canvasHeight = 800;
+  var canvas;
+
+  p.setup = function() {
+    canvas = p.createCanvas(canvasWidth, canvasHeight);
+    canvas.parent('canvas-container-1');
+    p.noLoop(); // Draw once since the artwork is static
+  };
+
+  p.draw = function() {
+    p.background(245, 232, 170);
+    drawPrimitives();
+  };
+
+  function drawPrimitives() {
+
+    // White circles
+    p.fill(255);
+    p.stroke(255);
+
+    p.circle(0, 0, 400);
+    p.circle(0, 400, 400);
+    p.circle(0, 800, 400);
+
+    p.circle(400, 0, 400);
+    p.circle(400, 400, 400);
+    p.circle(400, 800, 400);
+
+    p.circle(800, 0, 400);
+    p.circle(800, 400, 400);
+    p.circle(800, 800, 400);
+
+    // Orange circles
+    p.fill(245, 163, 62);
+    p.stroke(245, 163, 62);
+
+    p.circle(0, 0, 300);
+    p.circle(0, 400, 300);
+    p.circle(0, 800, 300);
+
+    p.circle(400, 0, 300);
+    p.circle(400, 400, 300);
+    p.circle(400, 800, 300);
+
+    p.circle(800, 0, 300);
+    p.circle(800, 400, 300);
+    p.circle(800, 800, 300);
+  }
+
+}; // <-- closes sketch1
+
+// Create the instance
+var myp5_1 = new p5(sketch1, 'canvas-container-1');
---
+++ sketch-2d-2.js
@@ -0,0 +1,51 @@
+// Mouse Trail Sketch - using p5.js instance mode
+var sketch2 = function(p) {
+
+  // All variables are scoped to this instance
+  var trails = [];
+  var topColor;
+  var bottomColor;
+
+  p.setup = function() {
+    // Create the canvas and attach it to the container
+    var canvas = p.createCanvas(800, 800);
+    canvas.parent('canvas-container-2');
+
+    p.noStroke();
+
+    topColor = p.color(255, 120, 0);
+    bottomColor = p.color(255, 50, 150);
+  };
+
+  p.draw = function() {
+
+    // Draw gradient background
+    for (var y = 0; y < p.height; y++) {
+      var percent = p.map(y, 0, p.height, 0, 1);
+      var lineColor = p.lerpColor(topColor, bottomColor, percent);
+
+      p.stroke(lineColor);
+      p.line(0, y, p.width, y);
+    }
+
+    // Draw mouse trails
+    p.noStroke();
+
+    trails.push(p.createVector(p.mouseX, p.mouseY));
+
+    if (trails.length > 100) {
+      trails.shift();
+    }
+
+    for (var i = 0; i < trails.length; i++) {
+      var pos = trails[i];
+
+      p.fill(255, 255, 255, 150);
+      p.circle(pos.x, pos.y, i / 2);
+    }
+  };
+
+};
+
+// Create the instance
+var myp5_2 = new p5(sketch2, 'canvas-container-2');
