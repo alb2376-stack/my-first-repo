eb915828fd14ce8a4d2c053b615cfb939ff02edf
alb2376-stack <alb2376@columbia.edu>
2026-07-08 23:43:16 -0400

    update

--- 2d-drawing.js
+++ 2d-drawing.js
@@ -2,8 +2,7 @@
 var sketch1 = function(p) {
   // All variables are scoped to this instance
   var canvasWidth = 800;
-  var canvasHeight = 400;
-  var gridSpacing = 40;
+  var canvasHeight = 800;
   var canvas;
 
   p.setup = function() {
@@ -12,38 +11,38 @@ var sketch1 = function(p) {
   };
 
   p.draw = function() {
-    p.background(250);
+    p.background(245,232,170);
     drawGrid();
     drawPrimitives();
   };
 
-  function drawGrid() {
-    p.stroke(200);
-    p.strokeWeight(1);
-    for (var x = 0; x <= p.width; x += gridSpacing) {
-      p.line(x, 0, x, p.height);
-    }
-    for (var y = 0; y <= p.height; y += gridSpacing) {
-      p.line(0, y, p.width, y);
-    }
-  }
-
   function drawPrimitives() {
-    // Rectangle
-    p.fill(255, 100, 100);
-    p.rect(120, 80, 100, 60);
-    // Ellipse
-    p.fill(100, 180, 255);
-    p.ellipse(350, 200, 90, 90);
-    // Line
-    p.stroke(80, 200, 120);
-    p.strokeWeight(4);
-    p.line(500, 100, 700, 300);
-    // Triangle
-    p.noStroke();
-    p.fill(255, 220, 80);
-    p.triangle(600, 80, 750, 60, 700, 200);
-  }
+      //orange background
+    background(245,232,170);
+
+    fill("white")
+    stroke("white")
+    circle(0,0,400)
+    circle(0,400,400)
+    circle(0,800,400)
+    circle(400,0,400)
+    circle(400,400,400)
+    circle(400,800,400)
+    circle(800,800,400)
+    circle(800,400,400)
+    circle(800,0,400)
+
+    fill(245,163,62)
+    stroke(245,163,62)
+    circle(0,0,300)
+    circle(0,400,300)
+    circle(0,800,300)
+    circle(400,0,300)
+    circle(400,400,300)
+    circle(400,800,300)
+    circle(800,800,300)
+    circle(800,400,300)
+    circle(800,0,300)
 };
 
 // Create the instance
